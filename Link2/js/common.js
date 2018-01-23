0634fed48a5062bc63937d65e1c66b0df0337c72
Sasha <oleksandr.gorychan@gmail.com>
2018-01-23 19:33:07 +0200

    last

---
+++ Link2/js/common.js
@@ -0,0 +1,10 @@
+(function () {
+        $('.header').load('/components/header.html');
+        $('.consuption_navigation').load('/components/navigation.html');
+        $('.popular_x').load('/components/popular.html');
+        $('.arrears_x').load('/components/arrears.html');
+        $('.footer').load('/components/footer.html');
+
+    })();
+
+
