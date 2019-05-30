f9b795b9ef394ec30e1e1aebf296d800d2b709cd
gorychan sasha <oleksandr.gorychan@gmail.com>
2019-05-31 01:01:03 +0300

    t

--- main.js
+++ main.js
@@ -14682,7 +14682,7 @@ __webpack_require__.r(__webpack_exports__);
 // The list of file replacements can be found in `angular.json`.
 var environment = {
     production: false,
-    serverSrc: 'http://kursova1403.herokuapp.com'
+    serverSrc: 'https://kursova1403.herokuapp.com'
 };
 /*
  * In development mode, to ignore zone related error stack frames such as
